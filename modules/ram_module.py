33dab3f44b7e018b379b191feee40ee4e44c8606
Глеб Попов <0504051@mail.ru>
2024-09-27 00:29:26 +0300

    update ram module + requirements.txt

--- modules/ram_module.py
+++ modules/ram_module.py
@@ -1,16 +1,19 @@
 import wmi
-from .memory_converter import correct_size
+from memory_converter import correct_size
 def get_rams():
     c = wmi.WMI()
     rams = c.Win32_PhysicalMemory()
     output = dict()
     for i, ram in enumerate(rams):
+        #print(ram)
         output[i] = {
             "name": ram.Name.strip(),
             "manufacturer": ram.Manufacturer,
             "speed": ram.Speed,
             "capacity": correct_size(int(ram.Capacity)),
-            "memory_type": ram.MemoryType
+            "memory_type": ram.MemoryType,
+            "part_number": ram.PartNumber.strip(),
+            "serial_number": ram.SerialNumber
         }
     return output
 #print(get_rams())
