60fb6cf4cd42bf395cb9d6888180d6080b79b404
Глеб Попов <0504051@mail.ru>
2024-09-30 00:10:18 +0300

    fix

--- modules/linux_tools/mainboard.py
+++ modules/linux_tools/mainboard.py
@@ -1,19 +1,23 @@
 import subprocess
 def getBaseboardProductName():
+    try:
         output = subprocess.check_output(f"sudo dmidecode -t 2 | grep 'Manufacturer\\|Product\\|Serial'", shell=True, text=True)
         lines = {}
         for line in output.strip().split('\n'):
             key, value = line.split(': ', 1)
             lines[key.strip()] = value.strip()
         return lines
+    except:
+        return None
 
 
 def get_mainboard():
     mainboard_info = getBaseboardProductName()
     output = list()
-    output.append({
-            "name": f"{mainboard_info['Manufacturer']} {mainboard_info['Product Name']}",
-            "serial_number": mainboard_info["Serial Number"]
-    })
+    if (mainboard_info != None):
+        output.append({
+                "name": f"{mainboard_info['Manufacturer']} {mainboard_info['Product Name']}",
+                "serial_number": mainboard_info["Serial Number"]
+        })
     return output
 print(getBaseboardProductName())
