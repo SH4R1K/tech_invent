93814f73216c9ed026f04d687f38dcfa8d028018
Глеб Попов <0504051@mail.ru>
2025-04-25 09:08:05 +0300

    fix post

--- api/api_module.py
+++ api/api_module.py
@@ -1,5 +1,8 @@
 import requests
 def send_data(json, api_url = "http://localhost:5239"):
     url = api_url + "/api"
-    response = requests.post(f"{url}/Invent", json=json)
+    #print(json)
+    headers = {'Content-Type': 'application/json; charset=utf-8'}
+    response = requests.post(f"{url}/Invent", data=json, headers=headers)
     print(response.status_code)
+    print(response.text)
