ca2948d6c6a9b95f30f0ed13e1093b432fac8793
Глеб Попов <0504051@mail.ru>
2025-05-12 10:55:47 +0300

    add index for ram

--- modules/windows_tools/ram_module.py
+++ modules/windows_tools/ram_module.py
@@ -4,16 +4,17 @@ def get_rams():
     c = wmi.WMI()
     rams = c.Win32_PhysicalMemory()
     output = list()
-    for ram in rams:
+    for idx, ram in enumerate(rams):
         #print(ram)
         output.append({
-            "name": ram.Name.strip(),
+            "name": f"[{idx+1}] {ram.Name.strip()}",
             "manufacturer": ram.Manufacturer,
             "speed": ram.Speed,
             "capacity": correct_size(int(ram.Capacity)),
             "memory_type": ram.MemoryType,
             "part_number": ram.PartNumber.strip(),
-            "serial_number": ram.SerialNumber
+            "serial_number": ram.SerialNumber,
+            "slot": ram.DeviceLocator.strip() 
         })
     return output
 #print(get_rams())
