7e055573e62c3d5f5bfea2d55ebc8682bfe5e03e
Глеб Попов <0504051@mail.ru>
2025-05-17 02:40:28 +0300

    aa

--- modules/linux_tools/disk_module.py
+++ modules/linux_tools/disk_module.py
@@ -10,7 +10,7 @@ def get_disks_info():
     for line in result.stdout.splitlines()[1:]:  
         match = re.match(r'(.+?)\s+(\S+)\s+(\S+)', line)
         if match:
-            model = match.group(1)
+            model = match.group(1).strip()
             size = match.group(2)
             device_type = match.group(3)
 
@@ -19,7 +19,7 @@ def get_disks_info():
             if device_type != 'disk':
                 continue
 
-            if len(model) == 0:
+            if not model:
                 continue
 
             size_gb = int(size[:-1]) if size[-1] == 'G' else int(size[:-1]) // 1024  # Преобразуем в ГБ
