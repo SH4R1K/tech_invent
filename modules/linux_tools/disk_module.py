391000e380f5309f41e33a013b591498a8932c17
Глеб Попов <0504051@mail.ru>
2025-05-17 02:12:05 +0300

    fix space between model name

--- modules/linux_tools/disk_module.py
+++ modules/linux_tools/disk_module.py
@@ -1,5 +1,6 @@
 import subprocess
 import json
+import re
 
 def get_disks_info():
     result = subprocess.run(['lsblk', '-o', 'NAME,MODEL,SIZE'], capture_output=True, text=True)
@@ -7,12 +8,12 @@ def get_disks_info():
     disks_info = []
     
     for line in result.stdout.splitlines()[1:]:  
-        parts = line.split()
-        if len(parts) >= 3:
-            name = parts[0]
-            model = parts[1]
-            size = parts[2]
-            size_gb = int(size[:-1]) if size[-1] == 'G' else int(size[:-1]) // 1024  
+        match = re.match(r'(\S+)\s+(.*)\s+(\S+)', line)
+        if match:
+            name = match.group(1)
+            model = match.group(2)
+            size = match.group(3)
+            size_gb = int(size[:-1]) if size[-1] == 'G' else int(size[:-1]) // 1024  # Преобразуем в ГБ
             disks_info.append({
                 'model': model,
                 'size': size_gb,
