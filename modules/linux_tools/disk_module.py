39ecf41d39591e8b53381270d1aeb0ed6b8ac02a
Глеб Попов <0504051@mail.ru>
2025-05-17 02:21:51 +0300

    update

--- modules/linux_tools/disk_module.py
+++ modules/linux_tools/disk_module.py
@@ -3,16 +3,23 @@ import json
 import re
 
 def get_disks_info():
-    result = subprocess.run(['lsblk', '-o', 'NAME,MODEL,SIZE'], capture_output=True, text=True)
+    result = subprocess.run(['lsblk', '-o', 'MODEL,SIZE,TYPE'], capture_output=True, text=True)
     
     disks_info = []
     
     for line in result.stdout.splitlines()[1:]:  
         match = re.match(r'(\S+)\s+(.*)\s+(\S+)', line)
         if match:
-            name = match.group(1)
-            model = match.group(2)
-            size = match.group(3)
+            model = match.group(1)
+            size = match.group(2)
+            device_type = match.group(3)
+
+            if device_type != 'disk':
+                continue
+
+            if len(model) == 0:
+                continue
+
             size_gb = int(size[:-1]) if size[-1] == 'G' else int(size[:-1]) // 1024  # Преобразуем в ГБ
             disks_info.append({
                 'model': model,
